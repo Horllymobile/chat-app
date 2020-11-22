650ab214c2dfc6a93e2407c84c4389c994bcd8a7
James Abraham Olamide <horlamidex1@gmail.com>
2020-11-22 12:53:42 +0100

    Establish socket io connections

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
-
 app.use(express.static(publicPath));
+
+let server = http.createServer(app);
+
+let io = socketIo(server);
+
+
+io.on('connection', (socket) => {
+    console.log('Connections established for ', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client is disconnected', socket.id);
+    })
+});
+
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`App is running on ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+});
 
 
 
